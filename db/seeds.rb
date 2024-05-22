4ed9a3307b8cffa7cfdbdd9ce50a8c49c383a512
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-22 12:48:00 -0400

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+hang = {name: "Hang Bar", address: "686 Notre Dame O", phone_number: "514-910-2227", category: "Vietnamese"}
+modavie = {name: "Modavie", address: "1 rue St-Paul O", phone_number: "514-287-9582", category: "French"}
+mignon = {name: "Mignon Steak", address: "101 rue St-Paul O", phone_number: "514-844-9663", category: "Steakhouse"}
+guillaume = {name: "Guillaume", address: "5170 St-Laurent", phone_number: "514-507-3199", category: "Bakery"}
+osteria = {name: "Osteria MKT", address: "1333 Robert-Bourassa", phone_number: "514-849-2044", category: "Italian"}
+
+[hang, modavie, mignon, guillaume, osteria].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
